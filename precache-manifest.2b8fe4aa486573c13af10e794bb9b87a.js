74a0d1d2a0d51e5cf85f2319e6afca2d2887e1d4
arunsnd2017 <arunsinghsnd@gmail.com>
2020-04-29 00:43:38 +0530

    Updates

---
+++ precache-manifest.2b8fe4aa486573c13af10e794bb9b87a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115ef007cba2575de46226c712d3d935",
+    "url": "/facedetecting-webapp/index.html"
+  },
+  {
+    "revision": "bdc446957e5e78a4bba2",
+    "url": "/facedetecting-webapp/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c7808c11360ba10b1a85",
+    "url": "/facedetecting-webapp/static/css/main.4867ee41.chunk.css"
+  },
+  {
+    "revision": "bdc446957e5e78a4bba2",
+    "url": "/facedetecting-webapp/static/js/2.81b8f66d.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facedetecting-webapp/static/js/2.81b8f66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7808c11360ba10b1a85",
+    "url": "/facedetecting-webapp/static/js/main.68bf93a9.chunk.js"
+  },
+  {
+    "revision": "dcccd103950cd3ea0489",
+    "url": "/facedetecting-webapp/static/js/runtime-main.159f1f45.js"
+  }
+]);
